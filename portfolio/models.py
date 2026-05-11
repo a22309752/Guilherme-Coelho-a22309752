fbac036dea24f9414b6007002e17d201e6d9bfd8
Guilherme Coelho <guiqcoelho23@gmail.com>
2026-05-11 13:55:49 +0000

    Adiciona as entidades e as devidas relações

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,131 +1,152 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
-
 class Licenciatura(models.Model):
     nome = models.CharField(max_length=100)
-    descricao = models.TextField()
+    descrição = models.TextField()
+    requisitos = models.CharField(max_length=100)
     duracao = models.CharField(max_length=100)
-    url_pagina_oficial = models.URLField()
-    requisitos = models.TextField()
+    url_oficial = models.URLField(blank= True, null=True)
 
     class Meta:
         verbose_name = "Licenciatura"
         verbose_name_plural = "Licenciaturas"
-
+         
     def __str__(self):
         return self.nome
 
 
 class Docente(models.Model):
-    nome = models.CharField(max_length=200)
+    nome = models.CharField(max_length=100)
     email = models.EmailField()
-    pagina_pessoal_url = models.URLField()
+    url_oficial = models.URLField(blank= True, null=True)
 
     class Meta:
         verbose_name = "Docente"
         verbose_name_plural = "Docentes"
-
+    
     def __str__(self):
         return self.nome
 
 
 class Unidade_Curricular(models.Model):
-    nome = models.CharField(max_length=200)
+    nome = models.CharField(max_length=100)
     ano_curricular = models.IntegerField()
     semestre = models.IntegerField()
-    url_uc = models.URLField()
-    imagem_url = models.URLField()
-    licenciatura = models.ForeignKey(Licenciatura, on_delete=models.CASCADE, related_name= "UCs")
+    url_oficial = models.URLField(blank= True, null=True)
+    imagem = models.ImageField(upload_to="media/")
+    licenciatura = models.ForeignKey(Licenciatura, on_delete=models.CASCADE, related_name= "UCs",blank= True, null=True)
     docente = models.ManyToManyField(Docente, related_name= "UCs")
-
+    
     class Meta:
-        verbose_name = "Unidade Curricular"
+        verbose_name = "Unicade Curricular"
         verbose_name_plural = "Unidades Curriculares"
+    
+    def __str__(self):
+        return self.nome
 
+
+
+class Tecnologia(models.Model):
+    nome = models.CharField(max_length=100)
+    url_oficial = models.URLField(blank= True, null=True)
+    logo_oficial = models.ImageField(upload_to= "media/")
+    descricao = models.TextField(blank=True,null=True)
+    nivel_interesse = models.IntegerField(validators=[MinValueValidator(0),
+            MaxValueValidator(10)])
+    
+    class Meta:
+        verbose_name = "Tecnologia"
+        verbose_name_plural = "Tecnologias"
+    
     def __str__(self):
         return self.nome
 
 
 class Competencia(models.Model):
-    nome = models.CharField(max_length=200)
-
+    nome = models.CharField(max_length=100)
+    
     class Meta:
         verbose_name = "Competência"
         verbose_name_plural = "Competências"
-
-    def __str__(self):
-        return self.nome
-
-
-class Tecnologia(models.Model):
-    nome =  models.CharField(max_length=100)
-    url_oficial = models.URLField()
-    descricao = models.TextField()
-    logo_url = models.URLField()
-    nivel_interesse = models.IntegerField()
-    nivel_proficiencia = models.IntegerField()
     
-    class Meta:
-        verbose_name = "Tecnologia"
-        verbose_name_plural = "Tecnologias"
-
     def __str__(self):
         return self.nome
 
 
 class Projeto(models.Model):
-    nome = models.CharField(max_length=200)
-    descricao = models.TextField()
-    imagem_url = models.URLField()
-    link_projeto = models.URLField()
+    nome = models.CharField(max_length=100)
     ano_curricular = models.IntegerField()
-    uc = models.ForeignKey(Unidade_Curricular, on_delete=models.CASCADE, related_name="projetos")
+    semestre = models.IntegerField()
+    descricao = models.TextField(blank=True,null=True)
+    imagem = models.ImageField(upload_to="media/")
+    conceitos_aplicados = models.CharField(max_length= 100)
+    uc = models.ForeignKey(Unidade_Curricular, on_delete=models.CASCADE, related_name="projetos",blank= True, null=True)
     tecnologia = models.ManyToManyField(Tecnologia, related_name="projetos")
     competencia = models.ManyToManyField(Competencia, related_name="projetos")
 
     class Meta:
         verbose_name = "Projeto"
         verbose_name_plural = "Projetos"
-
+    
     def __str__(self):
         return self.nome
 
 
+
 class Formacao(models.Model):
-    nome = models.CharField(max_length=200)
-    instituicao = models.CharField(max_length=200)
-    descricao = models.TextField()
+    nome = models.CharField(max_length=100)
+    url_oficial = models.URLField(blank= True, null=True)
+    logo_oficial = models.ImageField(upload_to= "media/")
+    descrição = models.TextField()
     data_inicio = models.DateField()
     data_fim = models.DateField()
-
+    
     class Meta:
-        verbose_name = "Formação"
+        verbose_name = "Formacao"
         verbose_name_plural = "Formações"
-
+    
     def __str__(self):
         return self.nome
 
 
 class MakingOf(models.Model):
-    titulo = models.CharField(max_length=200)
-    descricao = models.TextField()
-    imagem_url = models.ImageField(upload_to='portfolio/makingof', null=True, blank=True)
-    observacao = models.TextField(blank=True, null=True)
+ 
+    entidades = [
+        ("licenciatura", "Licenciatura"),
+        ("unidade_curricular", "Unidade Curricular"),
+        ("docente", "Docente"),
+        ("projeto", "Projeto"),
+        ("tecnologia", "Tecnologia"),
+        ("competencia", "Competência"),
+        ("formacao", "Formação"),
+        ("tfc", "TFC"),  
+    ]
+
+    titulo = models.CharField(max_length=100)
+    entidade = models.CharField(max_length=100,choices=entidades,default="nenhuma")
+    descricao_trabalho = models.TextField()
+    decisoes_tomadas = models.TextField()
+    erros_encontrados = models.TextField(blank=True)
+    correcoes_realizadas = models.TextField(blank=True)
+    imagem1 = models.ImageField(upload_to="makingof/", blank=True, null=True)
+    imagem2 = models.ImageField(upload_to="makingof/", blank=True, null=True)
 
     class Meta:
-        verbose_name = "Making Of"
-        verbose_name_plural = "Making Of"
+        verbose_name = "MakingOf"
+        verbose_name_plural = "MakingOf"
 
     def __str__(self):
         return self.titulo
 
-
+    
 class TFC(models.Model):
     titulo = models.CharField(max_length=100)
     autor = models.CharField(max_length=100)
-    resumo = models.TextField()
-    imagem = models.URLField(blank=True, null=True)
+    url_oficial = models.URLField(blank= True, null=True)
+    descricao = models.TextField()
+    imagem = models.ImageField(upload_to="media/")
 
     class Meta:
         verbose_name = "TFC"
@@ -133,3 +154,6 @@ class TFC(models.Model):
 
     def __str__(self):
         return self.titulo
+
+
+    
